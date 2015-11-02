cb51486da34463b3be29b01f0756032ae6a018d3
Antti Slunga <Antti.Slunga@solita.fi>
2015-11-02 16:15:31 +0200

    First part

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,19 +2,25 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if x true false))
 
 (defn abs [x]
-  ":(")
+  (if (<= 0 x)
+    x
+    (* -1 x))
+  )
 
 (defn divides? [divisor n]
-  ":(")
+  (if (mod divisor divisor)
+    true
+    false)
+  )
 
 (defn fizzbuzz [n]
   ":(")
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
   ":(")
